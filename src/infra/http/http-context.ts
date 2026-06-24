9ead424279ecd38604907d09635c97770fac9dbc
George-Santos-Dev <george.santos.dev@gmail.com>
2026-06-23 21:49:39 -0300

    refactor: decouple controller service typing from concrete use case implementations

--- src/infra/http/http-context.ts
+++ src/infra/http/http-context.ts
@@ -3,7 +3,16 @@ import { Request } from "express";
 import { createInMemoryServices } from "../in-memory";
 
 export type AppServices = ReturnType<typeof createInMemoryServices>;
+type ExecutableOnly<T extends Record<string, { execute: unknown }>> = {
+  [Key in keyof T]: Pick<T[Key], "execute">;
+};
 
-export function getServices(request: Request): AppServices {
-  return (request.app.locals as { services: AppServices }).services;
+export interface HttpServices {
+  automobiles: ExecutableOnly<AppServices["automobiles"]>;
+  drivers: ExecutableOnly<AppServices["drivers"]>;
+  vehicleUsages: ExecutableOnly<AppServices["vehicleUsages"]>;
+}
+
+export function getServices(request: Request): HttpServices {
+  return (request.app.locals as { services: HttpServices }).services;
 }
